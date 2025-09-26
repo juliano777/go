62bcefb8da64062018cbdc33e038ff23dee29605
Juliano Atanazio <juliano777@gmail.com>
2025-09-26 11:35:43 -0300

    .

--- pg.go
+++ pg.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/spf13/pflag"
 )
 
+// Function to get environment variables
 func GetEnvVars() map[string]string {
 
 	// Array of PostgreSQL environment variables
@@ -25,6 +28,37 @@ func GetEnvVars() map[string]string {
 	for _, i := range pgVars {
 		pgVarsMap[i] = os.Getenv(i)
 	}
+
+	return pgVarsMap
+}
+
+// Function to get parameters
+func GetParams() map[string]string {
+
+	// Help variables
+	h_host := "Servidor do PostgreSQL (default vazio)"
+	h_port := "Porta do PostgreSQL (default 5432)"
+	h_user := "Usuário do PostgreSQL (default = usuário do SO)"
+	h_database := "Nome do banco (default = usuário do banco)"
+
+	// Flags curtas e longas
+	host := pflag.StringP("host", "H", "", h_host)
+	port := pflag.IntP("port", "p", 5432, h_port)
+	user := pflag.StringP("user", "U", os.Getenv("USER"), h_user)
+	database := pflag.StringP("database", "d", "", h_database)
+
+	pflag.Parse()
+
+	// Se não foi passado --database explicitamente → herda de --user (já parseado)
+	if !pflag.CommandLine.Changed("database") {
+		*database = *user
+	}
+
+	// Monta a connection string
+	connStr := fmt.Sprintf(
+		"host=%s port=%d dbname=%s user=%s",
+		*host, *port, *database, *user,
+	)
 
 	return pgVarsMap
 }
